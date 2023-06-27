9464e5158a682d77c800c5ec5a7e57781d92e7e2
Dheeraj <dheeraj.kumar@virtueanalytics.com>
2023-06-27 21:34:14 +0530

    new wigets

---
+++ lib/get-Api-getxUsed.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GetApi extends StatelessWidget {
+  const GetApi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GetApi"),
+      ),
+      body: ,
+    );
+  }
+}
